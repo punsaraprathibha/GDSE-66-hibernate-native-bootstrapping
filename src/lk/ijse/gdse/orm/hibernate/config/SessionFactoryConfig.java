572a2c8b8565fd800d9483edb974301d4f703e5e
punsaraprathibha <punsaraprathibhagpp@gmail.com>
2023-08-05 12:07:19 +0530

    Fix issue in session factory

--- src/lk/ijse/gdse/orm/hibernate/config/SessionFactoryConfig.java
+++ src/lk/ijse/gdse/orm/hibernate/config/SessionFactoryConfig.java
@@ -15,12 +15,31 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 public class SessionFactoryConfig {
 
     private static SessionFactoryConfig factoryConfig;
+    private final SessionFactory sessionFactory;
 
     /**
      * Defines default constructor as private
      * to avoid object creation of this class from outside
      */
-    private SessionFactoryConfig() {}
+    private SessionFactoryConfig() {
+        // Creates a Service Registry
+        StandardServiceRegistry serviceRegistry
+                = new StandardServiceRegistryBuilder()
+                .configure()
+                .build();
+
+        // Creates a Metadata Object
+        Metadata metadata = new MetadataSources(serviceRegistry)
+                .addAnnotatedClass(Customer.class)
+                .getMetadataBuilder()
+//                .applyImplicitNamingStrategy(
+//                        ImplicitNamingStrategyJpaCompliantImpl.INSTANCE)
+                .build();
+
+        // Creates the Session Factory
+        sessionFactory = metadata
+                .buildSessionFactory();
+    }
 
     /**
      * @return lk.ijse.gdse.orm.hibernate.config.SessionFactoryConfig
@@ -38,25 +57,6 @@ public class SessionFactoryConfig {
      * by following the steps of Native Bootstrapping
      */
     public Session getSession() {
-        // Creates a Service Registry
-        StandardServiceRegistry serviceRegistry
-                = new StandardServiceRegistryBuilder()
-                .configure()
-                .build();
-
-        // Creates a Metadata Object
-        Metadata metadata = new MetadataSources(serviceRegistry)
-                .addAnnotatedClass(Customer.class)
-                .getMetadataBuilder()
-                .applyImplicitNamingStrategy(
-                        ImplicitNamingStrategyJpaCompliantImpl.INSTANCE)
-                .build();
-
-        // Creates the Session Factory
-        SessionFactory sessionFactory = metadata
-                .buildSessionFactory();
-
-
         // Opens a new Session and Returns
         return sessionFactory.openSession();
     }
